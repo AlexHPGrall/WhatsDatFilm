949241a51901bdaedf6962722f56e7c5d08a769a
Antoine Dausque <antoine.dausque@hotmail.fr>
2024-03-18 12:41:47 +0100

    fonction de tri du tableau

--- views/footer.php
+++ views/footer.php
@@ -1,14 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Backoffice</title>
-    <link rel="stylesheet" href="/views/style/styles.css">
-</head>
-
 <footer>
     <div class="footerCenter">
         <p>Whats Dat Film - 2024 - Bergeron, Grall, Dausque</p>
--- views/header.php
+++ views/header.php
@@ -1,14 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Backoffice</title>
-    <link rel="stylesheet" href="/views/style/styles.css">
-</head>
-
 <header>
     <div class="headerLeft"></div>
     <div class="headerCenter">
