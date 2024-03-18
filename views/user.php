46104b2042ce4a2a72198ec337a586eba1132159
Antoine Dausque <antoine.dausque@hotmail.fr>
2024-03-18 10:04:19 +0100

    antoine qui push

--- views/user.php
+++ views/user.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="utf-8" />
     <title>Tableau utilisateur</title>
@@ -15,45 +16,44 @@
                 <th>login</th>
                 <th>password</th>
                 <th>first name</th>
-                <th>last name</th> 
+                <th>last name</th>
                 <th>e-mail</th>
             </tr>
         </thead>
         <tbody>
-            <?php 
-                foreach($table as $userData)
-                {
-                    echo('<tr>');
-                    echo('<td>');
-                    echo $userData['userId'];
-                    echo('</td>');
-                    echo('<td>');
-                    echo $userData['userLogin'];
-                    echo('</td>');
-                    echo('<td>');
-                    echo $userData['userPassword'];
-                    echo('</td>');
-                    echo('<td>');
-                    echo $userData['userFirstName'];
-                    echo('</td>');
-                    echo('<td>');
-                    echo $userData['userLastName'];
-                    echo('</td>');
-                    echo('<td>');
-                    echo $userData['userEmail'];
-                    echo('</td>');
-                    echo('<td>');
-                    echo '<form action="/Admin/Delete" method="post">
-                    <button class="button" type="submit" name="userId" value="';
-                    echo $userData["userId"];
-                    echo '">Supprimer</button> </form>';
-                    echo '<form action="/Admin/User/Edit" method="post">
+            <?php
+            foreach ($table as $userData) {
+                echo ('<tr>');
+                echo ('<td>');
+                echo $userData['userId'];
+                echo ('</td>');
+                echo ('<td>');
+                echo $userData['userLogin'];
+                echo ('</td>');
+                echo ('<td>');
+                echo $userData['userPassword'];
+                echo ('</td>');
+                echo ('<td>');
+                echo $userData['userFirstName'];
+                echo ('</td>');
+                echo ('<td>');
+                echo $userData['userLastName'];
+                echo ('</td>');
+                echo ('<td>');
+                echo $userData['userEmail'];
+                echo ('</td>');
+                echo ('<td>');
+                echo '<form action="/Admin/Delete" method="post">
                     <button type="submit" name="userId" value="';
-                    echo $userData["userId"];
-                    echo '">Edit</button> </form>';
-                    echo('</td>');
-                    echo('</tr>');        
-                }
+                echo $userData["userId"];
+                echo '">Supprimer</button> </form>';
+                echo '<form action="/Admin/User/Edit" method="post">
+                    <button type="submit" name="userId" value="';
+                echo $userData["userId"];
+                echo '">Modifier</button> </form>';
+                echo ('</td>');
+                echo ('</tr>');
+            }
             ?>
         <tbody>
     </table>
