d8d7551f548d6f87e52972a95c8b0ad39d952020
Antoine Dausque <antoine.dausque@hotmail.fr>
2024-05-21 13:27:36 +0200

    aled

--- views/js/search.js
+++ views/js/search.js
@@ -30,8 +30,7 @@ $(document).ready(function () {
     }
 
     function displayResults(data) {
-        var resultsContainer = $('#results');
-        resultsContainer.empty();
+        var results = "";
         data = JSON.parse(data);
         if (data && data["results"] && data["results"].length > 0) {
             
@@ -39,9 +38,10 @@ $(document).ready(function () {
                 var imageUrl = movie.poster_path ? 'https://image.tmdb.org/t/p/w500' + movie.poster_path : 'no-image.jpg';
                 var title = movie.title;
                 var releaseYear = movie.release_date ? '(' + movie.release_date.substring(0, 4) + ')' : '(Date inconnue)';
-                var listItem = '<li><p><strong>' + title + '</strong> ' + releaseYear + '</p><img src="' + imageUrl + '" alt="' + title + '"><button class="add-btn" data-id="' + movie.id + '">Ajouter à la base de données</button></li>';
+                var listItem = '<li><p><strong>' + title + '</strong> ' + releaseYear + '</p><img src="' + imageUrl + '" alt="' + title + '"><div>' + movie.overview + '</div><button class="add-btn" data-id="' + movie.id + '">Ajouter à la base de données</button></li>';
+                results += listItem;
                 var movieData = {
-                    id : movie.id,
+                    id: movie.id,
                     title: movie.title,
                     frenchTitle: null,
                     runtime: null,
@@ -50,26 +50,26 @@ $(document).ready(function () {
                     movieRating: movie.vote_average,
                 };
                 movies[movie.id] = movieData;
-                resultsContainer.append(listItem);
-                $('.add-btn').off('click');
-                $('.add-btn').on('click', function () {
-                    var movieId = $(this).data('id');
-                    var movieData = movies[movieId];
-                    addToDatabase(movieData);
-                    alert('Film ajouté à la base de données avec succès !');
-                });
             });
+            return results;
         } else {
-            resultsContainer.append('<li>Aucun résultat trouvé.</li>');
+            return '<li>Aucun résultat trouvé.</li>';
         }
     }
+
+    $(document).on('click', '.add-btn', function () {
+        console.log("BOUTON CLIQUÉ");
+        var movieId = $(this).data('id');
+        var movieData = movies[movieId];
+        addToDatabase(movieData);
+    });
 
     function addToDatabase(movieData) {
         console.log(movieData);
         $.ajax({
             url: '/api/tmdb.php',
             type: 'POST',
-            data: {functionname: 'add', arguments: [movieData]},
+            data: { functionname: 'add', arguments: [movieData] },
             success: function () {
                 alert('Film ajouté à la base de données avec succès !');
             },
--- views/testapiview.php
+++ views/testapiview.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Recherche de films</title>
-<<<<<<< HEAD
     <link rel="stylesheet" href="/views/style/styles.css">
 </head>
 <body>
@@ -21,19 +20,6 @@
 </div>
 
 <?php require 'footer.php' ?>
-=======
-    <link rel="stylesheet" href="/views/addmovie.css">
-</head>
-<body>
-
-<div class="container">
-    <div class="search-bar">
-        <input type="text" id="searchInput" placeholder="Entrez le titre du film">
-    </div>
-    <div class="list-group"></div>
-</div>
-
->>>>>>> origin/main
 
 <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
 <script src="/views/js/search.js"></script>
