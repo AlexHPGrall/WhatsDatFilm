0fa207ed433d4c7c7b28715516073228a693254b
MathieuBer <106163741+MathieuBer@users.noreply.github.com>
2024-05-21 16:41:21 +0200

    add_movie_details

--- views/user.php
+++ views/user.php
@@ -16,26 +16,24 @@
         </thead>
         <tbody>
             <?php 
-                foreach($table as $userData)
-                {
-                    echo '<tr>',
-                        '<td>'.$userData['userId'].'</td>',
-                        '<td>'.$userData['userLogin'].'</td>',
-                        '<td>'.$userData['userPassword'].'</td>',
-                        '<td>'.$userData['userFirstName'].'</td>',
-                        '<td>'.$userData['userLastName'].'</td>',
-                        '<td>'.$userData['userEmail'].'</td>',
-                        '<td>',
-                        '<form action="/Admin/User/Delete" method="post" name="delete">
-                            <button type="submit" name="userId" value="'.$userData["userId"].'">Supprimer</button> 
-                        </form>',
-                '<form action="/Admin/User/Edit" method="post">
-                            <button type="submit" name="userId" value="' . $userData["userId"] . '">Edit</button> 
-                        </form>',
-                '</td>',
-                '</tr>';
-            }
-            ?>
+                foreach($table as $userData): ?>
+                        <tr>
+                        <td><?= $userData['userId'] ?></td>
+                        <td><?= $userData['userLogin'] ?></td>
+                        <td><?= $userData['userPassword'] ?></td>
+                        <td><?= $userData['userFirstName'] ?></td>
+                        <td><?= $userData['userLastName'] ?></td>
+                        <td><?= $userData['userEmail'] ?></td>
+                        <td>
+                        <form action="/Admin/User/Delete" method="post" name="delete">
+                            <button type="submit" name="userId" value="<?= $userData["userId"] ?>">Supprimer</button> 
+                        </form>
+                <form action="/Admin/User/Edit" method="post">
+                            <button type="submit" name="userId" value="<?= $userData["userId"] ?>">Edit</button> 
+                        </form>
+                </td>
+                </tr>
+            <?php endforeach; ?>
         <tbody>
     </table>
 
