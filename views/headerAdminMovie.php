6eec8c21fccf4bd463acab664ed4d4c0e1930fef
Antoine Dausque <antoine.dausque@hotmail.fr>
2024-05-29 13:43:20 +0200

    derniers boutons

--- views/headerAdminMovie.php
+++ views/headerAdminMovie.php
@@ -2,11 +2,11 @@
 <html lang="fr">
 
 <form action="/gameController/home" method="post">
-    <button class="btn" type="submit" id="game">Game</button>
+    <button class="btn" type="submit" id="game">Jeu</button>
 </form>
 <form action="/admin/home" method="post">
     <button class="btn" type="submit" id="home">Home</button>
 </form>
 <form action="/admin/user" method="post">
-    <button class="btn" type="submit" id="user">Users</button>
+    <button class="btn" type="submit" id="user">Utilisateurs</button>
 </form>
--- views/headerUser.php
+++ views/headerUser.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html lang="fr">
 
+<form action="/admin/home" method="post">
+    <button class="btn" type="submit" id="home">Backoffice</button>
+</form>
 <form action="/gameController/home" method="post">
-    <button class="btn" type="submit" id="game">Game</button>
+    <button class="btn" type="submit" id="game">Jeu</button>
 </form>
