5c2cf9150a0faeaefd89b2beeac646bf60ff68cf
Antoine Dausque <antoine.dausque@hotmail.fr>
2024-06-03 14:44:05 +0200

    icone en guise de boutons

--- views/headerAdminUser.php
+++ views/headerAdminUser.php
@@ -4,11 +4,11 @@
 <!-- Boutons du header quand on est sur la partie d'administration des utilisateurs -->
 
 <form action="/game/home" method="post">
-    <button class="btn" type="submit" id="game">Jeu</button>
+    <button class="logBtn" type="submit" id="game" title="Jeu"><img src="/views/style/jeu.png" alt="Jeu" class="logBtnContent"></button>
 </form>
 <form action="/admin/home" method="post">
-    <button class="btn" type="submit" id="home">Home</button>
+    <button class="logBtn" type="submit" id="home" title="Home"><img src="/views/style/home.png" alt="Home" class="logBtnContent"></button>
 </form>
 <form action="/admin/movie" method="post">
-    <button class="btn" type="submit" id="movie">Films</button>
+    <button class="logBtn" type="submit" id="movie" title="Films"><img src="/views/style/film.png" alt="Films" class="logBtnContent"></button>
 </form>
